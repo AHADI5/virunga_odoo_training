a12498d9cc3fbf09ef41d60b1bb539cecdd8c04f
AHADI5 <gloireahadi9@gmail.com>
2024-10-21 11:00:28 +0200

    linking menus to actions

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,6 +3,11 @@
     "version": "17.0.1.0.0",
     "license": "OEEL-1",
     "depends": ["base"],
+    
+    "data" : [
+        "views/estate_menu.xml" , 
+        "views/estate_property_views.xml"        
+    ] , 
 
     "application": True,
 }
