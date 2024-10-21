661f7154256d48eec26feee00b09de52ecbaf2b5
AHADI5 <gloireahadi9@gmail.com>
2024-10-21 11:22:21 +0200

    view data (duplicated data keyword )  bug fixing

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -15,16 +15,16 @@
      
      "data" : [
          #Security data 
-         "security/res_groups.xml"
+        "security/res_groups.xml"
+        
+        #views data 
+        "views/estate_property_views.xml"
+        "views/estate_menu.xml" , 
         
      ] , 
 
     
-    "data" : [
-        "views/estate_menu.xml" , 
-        "views/estate_property_views.xml"        
-    ] , 
-    
+  
     
     "application": True,
 }
