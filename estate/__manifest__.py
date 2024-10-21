987ac0f6d9771b54c9f15b042fd9574bdc39fccc
AHADI5 <gloireahadi9@gmail.com>
2024-10-21 10:01:22 +0200

    adding demo data to the module

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,6 +3,12 @@
     "version": "17.0.1.0.0",
     "license": "OEEL-1",
     "depends": ["base"],
+     
+     #data  
+     
+     "demo" : [
+       "demo/demo.xml"  
+     ],
 
     "application": True,
 }
