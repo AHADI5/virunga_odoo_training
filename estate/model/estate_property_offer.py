68e630565d5687a671970c99d119b74dde5435f5
AHADI5 <gloireahadi9@gmail.com>
2024-10-23 15:51:19 +0200

    fixing inconstancy bug

--- estate/model/estate_property_offer.py
+++ estate/model/estate_property_offer.py
@@ -26,9 +26,9 @@ class EstatePropertyOffer(models.Model):
         required=True
     )
     
-    property_type_id = fields.Char(
+    property_type_id = fields.Many2one(
         string='Property Type',
-        related='property_id.propety_type_id'  , 
+        related='property_id.property_type_id', 
         store=True
     )
     
