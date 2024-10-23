6d4d9e21abff75d3f6ce4f588341baeeb2be687c
AHADI5 <gloireahadi9@gmail.com>
2024-10-23 15:54:07 +0200

    Type of related field estate.property.offer.property_type_id is inconsistent with estate.property.property_type_id bug fixing by changing property_type_id in the estate_property_type_offer field type to Many2one

--- estate/model/estate_property_offer.py
+++ estate/model/estate_property_offer.py
@@ -1,7 +1,7 @@
 from odoo import fields , models
 
 class EstatePropertyOffer(models.Model):
-    property_type_id = fields.Char(
+    property_type_id = fields.Many2one(
         string='Property Type',
         related='property_id.propety_type_id'  , 
         store=True
