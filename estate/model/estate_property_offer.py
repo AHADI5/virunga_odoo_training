de6109b76031bcad678c7ef3105b7ea6a80630dc
AHADI5 <gloireahadi9@gmail.com>
2024-10-23 15:28:51 +0200

    setting the property_type_id as stored

--- estate/model/estate_property_offer.py
+++ estate/model/estate_property_offer.py
@@ -3,7 +3,8 @@ from odoo import fields , models
 class EstatePropertyOffer(models.Model):
     property_type_id = fields.Char(
         string='Property Type',
-        related='property_id.propety_type_id'
+        related='property_id.propety_type_id'  , 
+        store=True
     )
     
     
